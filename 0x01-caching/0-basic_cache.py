2e28f7d4988c2c8a1ab4cc9989349b98758ac518
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-02 23:57:06 +0200

    inherits from BaseCaching and is a caching system:
    You must use self.cache_data - dictionary from the parent class BaseCaching
    This caching system doesn’t have limit
    def put(self, key, item):
    Must assign to the dictionary self.cache_data the item value for the key key.
    If key or item is None, this method should not do anything.
    def get(self, key):
    Must return the value in self.cache_data linked to key.
    If key is None or if the key doesn’t exist in self.cache_data, return None.

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""This module contains a BasicCache which inherits
+from BaseCaching class and is a caching system.
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class definition"""
+
+    def __init__(self):
+        """Initiliazing access to the super class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Sets a key and value pair to the cache data property"""
+        if key or item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the value in self.cache_data linked to key."""
+        if key:
+            try:
+                return self.cache_data[key]
+            except KeyError:
+                return
+        return
---
+++ 0x01-caching/README.md
@@ -0,0 +1,18 @@
+# 0x01-caching
+
+## Description
+This is a repository for the 0x01-caching project. This project is part of the ALX software engineering program. The project is designed to help us learn and understand the concepts of caching.
+The project is designed to help us learn and understand the concepts of caching.
+
+## Objectives
+- What a caching system is
+- What FIFO means
+- What LIFO means
+- What LRU means
+- What MRU means
+- What LFU means
+- What the purpose of a caching system
+- What limits a caching system have
+
+## Technologies
+- Python
